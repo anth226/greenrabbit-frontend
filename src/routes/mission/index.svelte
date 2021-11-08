98efc01a663c0d12d5808709acd0e1382b840cda
DangThanhTrung1999 <pumaover@gmail.com>
2021-11-08 21:06:05 +0700

    Frame 119

--- src/routes/mission/index.svelte
+++ src/routes/mission/index.svelte
@@ -1,8 +1,10 @@
 <script>
-	import Mission from '../../components/mission/Mission.svelte';
+import Mission_119 from 'src/components/mission/Mission_119.svelte';
+import Mission from '../../components/mission/Mission.svelte';
 </script>
 
 <svelte:head>
 	<title>Main Hub | GreenRabbit Game</title>
 </svelte:head>
-<Mission />
+<!-- <Mission /> -->
+<Mission_119/>
